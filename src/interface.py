e91ec24ba5fc2d9bfa9b5e01cf7180abe611668d
shmonnorshmusie <shmonnorshmusie@aim.com>
2019-03-27 16:00:39 -0400

    Update interface

--- src/interface.py
+++ src/interface.py
@@ -1,5 +1,15 @@
 from tkinter import *
 
+def login():
+    username = entry_1.get()
+    password =
+
+    actuauser.retrivePassword()
+
+    if password == actualPass
+    return
+
+
 root = Tk()
 root.title("Homeless Registry Login")
 root.geometry("250x150")
@@ -10,15 +20,16 @@ label_2 = Label(root, text="Password")
 entry_1 = Entry(root)
 entry_2 = Entry(root, show="*")
 
-button_1 = Button(root, text="Login")
-button_2 = Button(root, text="Test")
+button_1 = Button(root, text="Login", command=login)
 
 label_1.grid(row=0, sticky=E)
 label_2.grid(row=1, sticky=E)
 entry_1.grid(row=0, column=1)
 entry_2.grid(row=1, column=1)
 button_1.grid(row=2, column=1)
-button_2.grid(row=2, column=2)
+
+
+
 
 
 root.mainloop()
