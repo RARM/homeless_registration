c9481551268ba9ec8ff2087a386965d71f840228
Rodolfo Andrés Rivas Matta <rodolfo.andres.pro@gmail.com>
2019-03-27 21:55:17 -0400

    Editing database input

--- src/index.py
+++ src/index.py
@@ -8,4 +8,4 @@ database_path = "database.db" # Path to database file
 database = Database(database_path)
 
 if not (database.check()):
-    database.run('CREATE TABLE people (ID varchar(9), FirstName varchar(100), LastName varchar(100)), Height varchar(100), EyesColor varchar(100), HairColor varchar(100), Gender varchar(100), Ethnicity varchar(100),  RegisteredTime varchar(100), HashFinger varchar(100)')
+    database.run('CREATE TABLE people (UserID varchar(9), FirstName varchar(100), LastName varchar(100)), Height varchar(100), EyesColor varchar(100), HairColor varchar(100), Gender varchar(100), Ethnicity varchar(100),  RegisteredTime varchar(100), HashFinger varchar(100), PRIMARY KEY(UserID)' )
