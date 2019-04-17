fe1ac457cb640f7aa7b42011632ab8dedeaac408
Rodolfo A. Rivas Matta <rodolfo.andres.pro@gmai.com>
2019-04-17 15:53:30 -0400

    Organizing person class variables

--- src/person.py
+++ src/person.py
@@ -5,24 +5,35 @@ import sqlite3
 
 # Using the Person class. Person('first name', 'last name', [yyyy, mm, dd], ["Height", "Weight", "Eyes color", "Hair color", "Gender"], ethnicity)
 class Person:
-    def __init__(self, firstName, lastName, DOB, physicalFeatures, ethnicity): # Constructor
+    def __init__(self, firstName, lastName, DOB, physicalFeatures, ethnicity, uniqueID=NULL, signInTime=time.time()): # Constructor
+        # Settings
+        self.separatorStr = '~' # Separator for list to strings in database
+
+        # Class variables from arguments
         self.firstName = firstName
         self.lastName = lastName
         self.DOB = date(DOB[0], DOB[1], DOB[2]) # DOB = date(yyyy, mm, dd)
-        self.DOB_String = self.dateToString(DOB[0], DOB[1], DOB[2])
         self.physicalFeatures = physicalFeatures
-        self.physicalFeatures_String = self.stringFromList(physicalFeatures)
-        self.signInTime = time.time()
-        self.age = self.computeAge()
         self.ethnicity = ethnicity
-        self.uniqueID = self.generateID()
+        self.signInTime = signInTime
 
-        # Separator for list to strings in database
-        self.separatorStr = '~'
+        # Variables calculated with the class methods
+        self.DOB_String = self.dateToString(DOB[0], DOB[1], DOB[2])
+        self.age = self.computeAge()
+        self.physicalFeatures_String = self.stringFromList(physicalFeatures)
 
+        if uniqueID = NULL:
+            self.uniqueID = self.generateID()
+        else:
+            self.uniqueID = uniqueID
+
+
+
+    # Methods below ----------
     def computeAge(self): # Compute the age in years
         today = date.today()
         return today.year - self.DOB.year - ((today.month, today.day) < (self.DOB.month, self.DOB.day))
+
 
     def generateID(self): # Generate unique ID here
       randomNumber = str(random.randint(100, 999))
@@ -30,9 +41,11 @@ class Person:
       DOB = str(int(self.DOB.year) + int(self.DOB.month) + int(self.DOB.day))[3]
       return randomNumber + initials + DOB
 
+
     def stringFromList(self, my_list):
         resultant_string = my_list.split(self.separatorStr)
         return resultant_string
+
 
     def save(self, database): # The database argument has to be a connection
         cursor = database.cursor() # Creating the cursor
