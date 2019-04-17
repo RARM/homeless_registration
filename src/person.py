23fcbceba25d52edc7d8ff0602b4f5e95f5ff9e0
Rodolfo A. Rivas Matta <rodolfo.andres.pro@gmai.com>
2019-04-17 15:10:37 -0400

    Solving problem Person.uniqueID
    Solving: 'Person' object has no attribute 'firstName'.
    Person.generateID() has to be called after all variables that the
    function calls are declared.

--- src/person.py
+++ src/person.py
@@ -6,7 +6,6 @@ import sqlite3
 # Using the Person class. Person('first name', 'last name', [yyyy, mm, dd], ["Height", "Weight", "Eyes color", "Hair color", "Gender"], ethnicity)
 class Person:
     def __init__(self, firstName, lastName, DOB, physicalFeatures, ethnicity): # Constructor
-        self.uniqueID = self.generateID()
         self.firstName = firstName
         self.lastName = lastName
         self.DOB = date(DOB[0], DOB[1], DOB[2]) # DOB = date(yyyy, mm, dd)
@@ -16,15 +15,14 @@ class Person:
         self.signInTime = time.time()
         self.age = self.computeAge()
         self.ethnicity = ethnicity
+        self.uniqueID = self.generateID()
 
         # Separator for list to strings in database
         self.separatorStr = '~'
 
-
     def computeAge(self): # Compute the age in years
         today = date.today()
         return today.year - self.DOB.year - ((today.month, today.day) < (self.DOB.month, self.DOB.day))
-
 
     def generateID(self): # Generate unique ID here
       randomNumber = str(random.randint(100, 999))
@@ -32,11 +30,9 @@ class Person:
       DOB = str(int(self.DOB.year) + int(self.DOB.month) + int(self.DOB.day))[3]
       return randomNumber + initials + DOB
 
-
     def stringFromList(self, my_list):
         resultant_string = my_list.split(self.separatorStr)
         return resultant_string
-
 
     def save(self, database): # The database argument has to be a connection
         cursor = database.cursor() # Creating the cursor
@@ -57,10 +53,9 @@ class Person:
 #        return list
 
 
-# Test area
+# Test block
 person1 = Person("Mary", "Sue", [1980, 5, 1], ["5'1", "120", "Blue", "Brown", "Female"], "White")
-
-
+print(person1.uniqueID)
 
 flag = False
 hairColor = [""] * (10)
